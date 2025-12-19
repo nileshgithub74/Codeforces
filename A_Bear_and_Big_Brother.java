9551fa73756afeb860ff7e25beb0454d1f53c4b5
Nilesh Kumar <kumarnilesh843127@gmail.com>
2025-12-20 00:57:33 +0530

    code it

---
+++ A_Bear_and_Big_Brother.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+ public class A_Bear_and_Big_Brother {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            // Your code for each test case goes here
+            
+
+
+            
+        }
+        sc.close();
+    }
+}
--- PetyaAndString.java
+++ PetyaAndString.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class PetyaAndString {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine().toLowerCase();
+        String s2 = sc.nextLine().toLowerCase();
+
+        int ans = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
+            int a = s1.charAt(i) - 'a';
+            int b = s2.charAt(i) - 'a';
+
+            if (a > b) {
+                ans = 1;
+                break;
+            } else if (a < b) {
+                ans = -1;
+                break;
+            }
+           
+        }
+
+        System.out.println(ans);
+        sc.close();
+    }
 }
