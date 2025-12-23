185b0db9ab2b5d572381549a5a227e96fe807d0e
Nilesh Kumar <kumarnilesh843127@gmail.com>
2025-12-23 16:00:13 +0530

    commit

--- A_Next_Round.java
+++ A_Next_Round.java
@@ -1,3 +1,24 @@
-public class A_Next_Round {
-    
+import java.util.Scanner;
+
+ public class A_Next_Round {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            // Your code for each test case goes here
+            
+            
+
+
+
+
+
+
+
+
+
+
+        }
+        sc.close();
+    }
 }
