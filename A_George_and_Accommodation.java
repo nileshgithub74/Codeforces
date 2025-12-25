3e0aa014f59717722d690c45aff927879f65b141
Nilesh Kumar <kumarnilesh843127@gmail.com>
2025-12-25 20:36:55 +0530

    commmit

---
+++ A_George_and_Accommodation.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+ public class A_George_and_Accommodation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while(t-- >0){
+
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+            
+
+
+
+        }
+
+
+      
+
+
+
+
+
+        sc.close();
+    }
+}
